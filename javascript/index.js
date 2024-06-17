99f62ac2d60582c3e14f4efd6a07422d4e51bcf2
VanyaMark <markova.vanya@gmail.com>
2024-06-17 08:40:12 +0200

    async await recipe done

--- javascript/index.js
+++ javascript/index.js
@@ -64,6 +64,7 @@ getInstruction(
                     document.querySelector(
                       "#mashedPotatoes"
                     ).innerHTML += `<li>Mashed potatoes are ready!</li>`;
+                    document.querySelector("#mashedPotatoesImg").style.display = "block";
                   },
                   (err) => console.log(err)
                 );
@@ -115,11 +116,36 @@ obtainInstruction("steak", 0)
   .then((step8) => {
     document.querySelector("#steak").innerHTML += `<li>${step8}</li>`;
     document.querySelector("#steak").innerHTML += `<li>Steak is ready!</li>`;
-    return obtainInstruction("steak", 8)
+    document.querySelector("#steakImg").style.display = "block";
   })
   .catch(err => console.log(err));
 // Iteration 3 using async/await
 // ...
+async function makeBroccoli() {
+  try {
+ const step1 = await obtainInstruction("broccoli", 0);
+  document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`
+  const step2 = await obtainInstruction("broccoli", 1);
+  document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`
+  const step3 = await obtainInstruction("broccoli", 2);
+  document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`
+  const step4 = await obtainInstruction("broccoli", 3);
+  document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`
+  const step5 = await obtainInstruction("broccoli", 4);
+  document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`
+  const step6 = await obtainInstruction("broccoli", 5);
+  document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`
+  const step7 = await obtainInstruction("broccoli", 6);
+  document.querySelector("#broccoli").innerHTML += `<li>${step7}</li>`
+  document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`
+  document.querySelector("#broccoliImg").style.display = "block";
+} 
+catch (err) {
+  console.log(err)
+}
+}
+
+makeBroccoli()
 
 // Bonus 2 - Promise all
 // ...
